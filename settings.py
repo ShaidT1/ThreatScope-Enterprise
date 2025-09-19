6edd3e2af0352cdcbd1d059aaa4ddbc5fd586d4d
ShaidsAnonymous <shaidt137@gmail.com>
2025-09-19 17:38:45 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
-# src/config/settings.py
-from dataclasses import dataclass
 
+from dataclasses import dataclass
+# are you having a good day? :)
 @dataclass
 class Settings:
     database_url: str =  "postgresql://threatscope:devpassword123@localhost:5432/threatscope_db"
-    environment: str = "development"          # "development" or "production"
+    environment: str = "development"  
     smtp_server: str = "smtp.example.com"
     email_user: str = "example@example.com"
     email_password: str = "password"
